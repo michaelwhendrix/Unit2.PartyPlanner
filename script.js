7795cc5712a98fbdfd1a9fda22d3ef1bd549d7a2
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-11-07 13:16:43 -0600

    added loop to log all names

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ const getAllArtist = async() => {
     main.appendChild(h2);
     const response = await fetch("https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/artists");
     const artistInfo = await response.json();
-    console.log(artistInfo);
-
+    
+    for(let i =0; i < artistInfo.data.length; i++){
+    console.log(artistInfo.data[i].name);
+    }
 }
 
 
