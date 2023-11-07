85adc3d1f85b3a4fe3c18c284eb975ff902faa18
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-11-07 13:38:02 -0600

    added list to webpage main

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const h2 = document.createElement('h2');
+const ol = document.createElement('ol');
+
 const main = document.querySelector('main');
 
 
-
 const getAllArtist = async() => {
-    h2.innerText = "Artists";
-    main.appendChild(h2);
+
+    
     const response = await fetch("https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/artists");
     const artistInfo = await response.json();
-    
+
+    h2.innerText = "Artists";
+    main.appendChild(h2);
+    main.appendChild(ol);
+
     for(let i =0; i < artistInfo.data.length; i++){
-    console.log(artistInfo.data[i].name);
+        const li = document.createElement('li');
+        li.innerText = artistInfo.data[i].name;
+        ol.appendChild(li);
     }
 }
 
