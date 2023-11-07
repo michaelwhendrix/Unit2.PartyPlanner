113a6f75e526b9963324831c2cf0005c86d64466
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-11-07 11:42:26 -0600

    getAllArtist with test of h2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('test');
+const h2 = document.createElement('h2');
+const main = document.querySelector('main');
+
+
+
+const getAllArtist = () => {
+    h2.innerText = "test";
+    main.appendChild(h2);
+}
+
+
+getAllArtist();
