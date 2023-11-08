0665ebeb1f5a8eb765f2757fdc55e27e3ce0c279
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-11-07 19:56:45 -0600

    following along demo to add functions up to renderDetails

--- script.js
+++ script.js
@@ -23,15 +23,47 @@ const getAllArtist = async() => {
     renderAllArtist();
 }
 
+
+//******function called by renderAllArtists to get the details of an artist
+const getArtistDetails = async (artistId) => {
+    const response = await fetch(`${baseURL}/artists/${artistId}`);
+    const jsonResponse = await response.json();
+    const artistDetails = jsonResponse.data;
+    renderDetails();
+}
+///****************************
+
+
+//******************************************
+const renderDetails = () => {
+
+}
+
+//***********funtion that is called be getAllArtists to render all artists in main on page
 const renderAllArtist = () => {
+
+    //adds title and the framework for ordered list of artits names
     h2.innerText = "Artists";
     main.appendChild(h2);
     main.appendChild(ol);
 
+    //makes new array with multiple list item tags and name from artists array
     const artistNames = state.allArtists.map((singleArtist) => {
         return `<li>${singleArtist.name}</li>`;
     });
+
+    //transforms the array generated above to a string for adding to page
     ol.innerHTML = artistNames.join('------');
     main.appendChild(ol);
+
+    // gets all list items from page and adds the eventListener
+    const listItems = document.querySelectorAll('li');
+    listItems.forEach((artistListItem) => {
+        artistListItem.addEventListener('click', (event) => {
+            getArtistDetails(event.target.id);
+        });
+    });
+
 }
+//**************
     getAllArtist();
