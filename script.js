afeff22312a849f137628a19f966360415c64284
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-11-07 18:33:07 -0600

    added new renderAllArtist according to second demo

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/*
+The baseURL is used to shorten the argument in the fetch function
+
+
+*/
+const baseURL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF`;
+
+const state ={
+    allArtists:[]
+}
 const h2 = document.createElement('h2');
 const ol = document.createElement('ol');
 const nameArtist = document.createElement('p');
@@ -6,37 +16,22 @@ const img = document.createElement('img');
 
 const main = document.querySelector('main');
 
-
 const getAllArtist = async() => {
+    const response = await fetch(`${baseURL}/artists`);
+    const jsonResponse = await response.json();
+    state.allArtists = jsonResponse.data;
+    renderAllArtist();
+}
 
-    
-    const response = await fetch("https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/artists");
-    const artistInfo = await response.json();
-
+const renderAllArtist = () => {
     h2.innerText = "Artists";
     main.appendChild(h2);
     main.appendChild(ol);
 
-    for(let i =0; i < artistInfo.data.length; i++){
-
-        const li = document.createElement('li');
-        li.innerText = artistInfo.data[i].name;
-
-        li.addEventListener('click', () => {
-        console.log(artistInfo.data[i]);
-        main.innerHTML = '';
-        nameArtist.innerText = artistInfo.data[i].name;
-        main.appendChild(nameArtist);
-        });
-
-        ol.appendChild(li);
-
-        }
-
-    
+    const artistNames = state.allArtists.map((singleArtist) => {
+        return `<li>${singleArtist.name}</li>`;
+    });
+    ol.innerHTML = artistNames.join('------');
+    main.appendChild(ol);
 }
-
-//const getSingleArtist = async(id) => {
-
-//}
-getAllArtist();
+    getAllArtist();
